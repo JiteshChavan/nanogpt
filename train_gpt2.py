1cb2e3a40f72410ef2985a28c31801d7a207af8a
Jitesh C <jchavan010@gmail.com>
2024-11-30 14:10:17 -0500

    ddp initialized

--- train_gpt2.py
+++ train_gpt2.py
@@ -275,24 +275,35 @@ class DataLoaderLite:
             self.current_position = 0
         return x, y
 # -----------------------------------------------------------------------------------------------------------------------------------------
+# run the training loop
+from torch.distributed import init_process_group, destroy_process_group
+import os
 
 # setup DDP (distributed data parallel).
 # torchrun command sets the env variables RANK, LOCAL_RANK, WORLD_SIZE
-import os
 ddp = int (os.environ.get ('RANK', -1)) != -1 # is this a ddp run?
 if ddp:
     # use of DDP atm demands cuda, we set the device appropriately according to rank
-    assert torch.cuda.is_available(). "for now I think we need CUDA for DDP"
-
-
-# attempt to auto detect the device
-device = "cpu"
-if torch.cuda.is_available():
-    device = "cuda"
-elif hasattr (torch.backends, "mps") and torch.backends.mps.is_available():
-    device = "mps"
-print (f"using device: {device}")
-#device = "cpu" # OVERRIDE
+    assert torch.cuda.is_available(), "for now I think we need CUDA for DDP"
+    init_process_group(backend='nccl')
+    ddp_rank = int (os.environ['RANK'])
+    ddp_local_rank = int (os.environ['LOCAL_RANK'])
+    ddp_world_size = int (os.environ['WORLD_SIZE'])
+    device = f'cuda:{ddp_local_rank}'
+    torch.cuda.set_device (device)
+    master_process = ddp_rank == 0 # this process will do the logging checkpoining etc
+else:
+    # vanilla, non-DDP run
+    ddp_rank = 0
+    ddp_local_rank = 0
+    master_process = True
+    # attempt to autodetect device
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        device = "mps"
+    print (f"using device: {device}")
 
 torch.manual_seed(1337)
 if torch.cuda.is_available():
